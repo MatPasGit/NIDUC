6e142e90d01519cd34dc8e1ad3f7476c5637c729
mpasierbek <matpasierbek@gmail.com>
2019-04-03 17:11:21 +0200

    dziwny ten python

--- niduc.py
+++ niduc.py
@@ -1,2 +1,13 @@
-msg ="hello"
-print(msg)
+import numpy as np
+import random
+
+def fillArray(array):
+ number=random.randint(0,1)
+
+
+def signalDisruption(array):
+    newarray=array
+    for f in array :
+    
+        return newarray
+
