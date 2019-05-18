1943af51842b12a7dacbfdc333ec30f3c5092497
mpasierbek <matpasierbek@gmail.com>
2019-05-18 19:33:52 +0200

    weird error in tests.py

--- tests.py
+++ tests.py
@@ -3,24 +3,30 @@ from niduc import signalDisruption
 from niduc import fillArray
 from niduc import signalReadrepeat
 import random
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def tests():
-    ones=0
-    zeros=0
-   
+    ones=0  #ilość przekłamać na jedynki
+    zeros=0 #ilosć przekłamań na zera
+    indexarray = [] #tablica indeksów do wykresu
     try :
-        length = (input('transmission lengh:'))
+        length = (input('Ile powieleń:'))
     except ValueError:
         print("Not a number")
-       
+
+    length=int(length)   
 
     try:
-        howmuch = (input('WIE VIELE TIMES:'))
+        howmuch = (input('Ile bitów :'))
     except ValueError:
         print("Not a number")
+    
+    howmuch=int(howmuch)
+    indexarray = [howmuch]  # tablica indeksów do wykresu
     #tablica testowa wypełniona losowo 
+    howmuch=int(howmuch)
     testarray=[]    
     for i in range(1,howmuch):
         weighted_random = [1] * 50 + [0] * 50 
@@ -29,11 +35,12 @@ def tests():
         else:
             testarray.append(0)
     #tablica z indeksami (do wykresu)
-    indexarray=[]
-    for i in range(0,howmuch):
+    for i in range(0,howmuch-1):
         indexarray[i]=i
-#tutaj zaczynamy testowanie 
-#pętla sprawdza sto razy ileś bitów
+
+    subindexarray=indexarray    
+    #tutaj zaczynamy testowanie 
+    #pętla sprawdza sto razy ileś bitów
     for m in range(0,99):
         stoparray = []
         for i in range(0,len(testarray)):
@@ -41,9 +48,23 @@ def tests():
             onebitarray = fillArray(length, bit)
             onebitarray=signalDisruption(onebitarray)
             newbit=signalReadrepeat(onebitarray)
-            stoparray.append(newbit)
+            stoparray.append(newbit)            #symulacja przesyłu i dopisanie do tablicy przesłanej
 
         for i in range(0,howmuch):
+            if testarray[i] != stoparray[i]:
+                indexarray[i]+=1
+                if testarray[i] == 0 :
+                    ones+=1
+                else : zeros +=1        #porównanie tablic i analityka 
+
+    plt.plot(subindexarray, indexarray, 'ro')
+    plt.axis([0, howmuch, 0, howmuch])
+    plt.show()
+
+
+
+tests()
+
 
 
 
